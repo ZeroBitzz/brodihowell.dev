5137e3706c05c4adcced503893f316cb2fe63a23
Broderick <zerothesu@gmail.com>
2022-10-31 22:41:52 -0600

    added project section, removed education section, fixed crt effect, and added time to footer.

--- assests/scripts/script.js
+++ assests/scripts/script.js
@@ -1,4 +1,14 @@
+// REMOVES LOADING PAGE AFTER IT IS FINISHED
 function removeLoadingPage(){
     document.getElementById("loader-wrapper").style.visibility = "hidden"
 }
 setTimeout(removeLoadingPage, 3000)
+
+// ADDS LIVE TIME IN BROWSER
+function refreshTime() {
+    const timeDisplay = document.getElementById("time");
+    const dateString = new Date().toLocaleString();
+    const formattedString = dateString.replace(", ", " - ");
+    timeDisplay.textContent = formattedString;
+}
+setInterval(refreshTime, 1000);
